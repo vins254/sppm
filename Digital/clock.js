8691bb11a4b1c73321fb9e2ef3e82027903615bf
vins254 <mainavincent003@gmail.com>
2024-10-19 17:00:10 +0300

    Added clock files

---
+++ Digital/clock.js
@@ -0,0 +1,15 @@
+function updateTime() {
+    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const now = new Date();
+    const day = now.getDay();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const {style} =document.documentElement;
+    style.setProperty('--timer-day', `'${daysOfWeek[day]}'`);
+    style.setProperty('--timer-hours', `'${hours}'`);
+    style.setProperty('--timer-minutes', `'${minutes}'`);
+    style.setProperty('--timer-seconds', `'${seconds}'`);
+}
+
+setInterval(updateTime, 1000);
